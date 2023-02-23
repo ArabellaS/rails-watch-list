690d2c3400c494a644463064e501214f624b5ec9
Cecile Saiz <cecile.saiz@gmail.com>
2023-02-23 16:34:07 +0100

    Watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+def new_movies
+  Movie.destroy_all
+  url = "http://tmdb.lewagon.com/movie/top_rated"
+  movies = URI.open(url).read
+  movie_list = JSON.parse(movies).transform_keys(&:to_sym)
+  flims = movie_list[:results].map {|movie|movie.transform_keys(&:to_sym) }
+  flims.each do |movie|
+    Movie.create!(title: movie[:original_title], overview: movie[:overview], rating: movie[:vote_average], poster_url: movie[:poster_path])
+  end
+end
+
+p new_movies
